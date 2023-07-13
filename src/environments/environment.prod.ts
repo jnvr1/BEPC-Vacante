dd5d662c05957dd11693e2a55a91dc79544a6361
jnvr1 <jonathan.viramontes.ramirez@gmail.com>
2023-07-12 21:47:04 -0600

    Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyALBhowwxavaLRCWIcRDnqEGYpC_wjoytY",
+    authDomain: "pruebabepc.firebaseapp.com",
+    databaseURL: "https://pruebabepc-default-rtdb.firebaseio.com",
+    projectId: "pruebabepc",
+    storageBucket: "pruebabepc.appspot.com",
+    messagingSenderId: "902329888399",
+    appId: "1:902329888399:web:1c5ccff5a1646328914afa"
+  }
 };
